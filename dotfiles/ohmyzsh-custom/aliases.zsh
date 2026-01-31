abc7e4bd8d5ff15e1f7775825562f60ddd9b2300
Leonard <leo.remus@me.com>
2026-01-31 02:04:12 +0100

    added aliases.zsh

---
+++ dotfiles/ohmyzsh-custom/aliases.zsh
@@ -0,0 +1,20 @@
+alias gits="git status"
+alias gitp="git pull"
+alias gita="git add"
+alias gitc="git commit -m"
+alias gitP="git push"
+
+alias yads="yadm status"
+alias yadp="yadm pull"
+alias yada="yadm add"
+alias yadc="yadm commit -m"
+alias yadP="yadm push"
+
+alias vim="nvim"
+
+alias python="python3.13"
+alias py="python3.13"
+
+alias rpi="ssh pirem@raspberrypi.local"
+
+alias k="kubectl"
