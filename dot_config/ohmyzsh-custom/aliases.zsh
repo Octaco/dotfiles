bd068d6e59bbe6267dbdd2bcb5ccf093952d7575
Leonard <leo.remus@me.com>
2026-05-24 00:00:04 +0200

    zsh aliases

---
+++ dot_config/ohmyzsh-custom/aliases.zsh
@@ -0,0 +1,31 @@
+alias gits="git status"
+alias gitp="git pull"
+alias gita="git add"
+alias gitc="git commit"
+alias gitP="git push"
+alias lg="lazygit"
+
+
+alias chs="chezmoi status"
+alias cha="chezmoi add"
+alias chA="chezmoi apply -v"
+alias chd="chezmoi diff"
+alias che="chezmoi edit"
+alias chcd="chezmoi cd"
+
+
+alias vim="nvim"
+alias vi="nvim"
+
+alias python="python3.13"
+alias py="python3.13"
+
+alias rpi="ssh pirem@raspberrypi.local"
+
+alias k="kubectl"
+
+# kittens
+
+alias icat="kitten icat"
+alias d="kitten diff"
+
